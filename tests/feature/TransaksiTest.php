f03438f3dc3b48251ee796fcdac52edcb7d916a0
Agatha <agathapuspaadellia@gmail.com>
2026-05-11 13:45:08 +0700

    update testing structure

--- tests/feature/TransaksiTest.php
+++ tests/feature/TransaksiTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace Tests\Feature;
 
 use CodeIgniter\Test\CIUnitTestCase;
 use CodeIgniter\Test\FeatureTestTrait;
--- tests/unit/UserModelTest.php
+++ tests/unit/UserModelTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace Tests\Unit;
 
 use CodeIgniter\Test\CIUnitTestCase;
 use App\Models\UserModel;
